ad67317a1a2e90e14c2e9110c35e03b8e1e8ba9c
Trevor Vallender <trevor@tsvallender.co.uk>
2026-02-10 16:30:08 +0000

    neovim cleanup

--- .config/nvim/lua/plugins/git-worktree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "ThePrimeagen/git-worktree.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim" },
-  config = function()
-    require('telescope').load_extension("git_worktree")
-    vim.keymap.set('n', '<leader>tw', require('telescope').extensions.git_worktree.git_worktrees, {})
-    vim.keymap.set('n', '<leader>tc', require('telescope').extensions.git_worktree.create_git_worktree, {})
-  end
-}
